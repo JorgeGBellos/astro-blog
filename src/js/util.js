848abc40549bf01a24538ac70aa768812ce74111
JorgeAGB <jorgegbellos@gmail.com>
2023-06-08 23:15:36 -0600

    Dynamic Routes, home, blog and categories sections updated

--- src/js/util.js
+++ src/js/util.js
@@ -14,3 +14,38 @@ export function formatDate(date) {
     timeZone: "UTC",
   });
 }
+
+export function formatBlogPosts(posts,{
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined,
+} = {}){
+  const filteredPosts = posts.reduce((acc,post) =>{
+    const {date,draft} = post.frontmatter;
+    // filterOutDrafts is true
+    if(filterOutDrafts && draft) return acc;
+
+    // filterOutFuturePosts is true
+    if(filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    // add post to acc
+    acc.push(post)
+
+    return acc;
+  }, [])
+
+  // sortByDate or randomize
+  if(sortByDate){
+    filteredPosts.sort((a,b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  if(typeof limit === "number"){
+    return filteredPosts.slice(0,limit);
+  }else{
+    return filteredPosts;
+  }
+
+}
